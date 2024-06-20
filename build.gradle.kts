c020aeeae364cacd0a0b07b72067bad1300d78c7
MohamedMohamed <m01126165016@icloud.com>
2024-06-21 01:51:58 +0300

    Update Data

--- build.gradle.kts
+++
@@ -1,7 +0,0 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
-    id("androidx.navigation.safeargs") version "2.7.7" apply false
-
-}
